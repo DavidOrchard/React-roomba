08aa7e0b3223965c634389a0dd68b4fa63db8265
David Orchard <david.orchard@dapperlabs.com>
2022-06-24 13:59:16 -0700

    efficient impl

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,15 +1,13 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders board, buttons, and roomba', () => {
   render(<App />);
-  const linkElement = screen.getByText(/roomba fun/i);
-  expect(linkElement).toBeInTheDocument();
-  const board = screen.getByText('Board');
-  expect(board).toBeInTheDocument();
   const move = screen.getByRole('button', {name: /move/i});
   expect(move).toBeInTheDocument();
+  const rotate = screen.getByRole('button', {name: /rotate/i});
+  expect(rotate).toBeInTheDocument();
+  expect(screen.getByText(/up/i)).toBeInTheDocument();
+  expect(screen.getByTestId('1-1')).toHaveTextContent('X');
+  expect(screen.getByTestId('0-0')).toHaveTextContent('');
 });
-
-// should have way more tests, such as edge & object detection
--- src/components/BoardData/BoardData.test.tsx
+++ src/components/BoardData/BoardData.test.tsx
@@ -1,7 +1,7 @@
 import {BoardData} from './BoardData';
 
 test('renders board with roomba 3,3', () => {
-    const board = BoardData({x:3, y:3});
+    const board = BoardData({x:3, y:3}, {x:4, y:4});
     expect(board[3][3]).toEqual('X');
     expect(board[0][0]).toEqual(' ');
 });
